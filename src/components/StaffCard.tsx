542377fb041346281b20b3e18da31ee5bcc8414f
Nicolás González S. <nicoa.gonzalez@duocuc.cl>
2025-10-11 10:58:58 -0300

    opt images

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -10,7 +10,7 @@ export default function StaffCard({ nombre, cargo, imagen }: Props) {
       <img
         src={imagen}
         alt={nombre}
-        className="w-full h-[400px] object-cover"
+        className="w-full h-[400px] object-cover flex items-center justify-center text-white font-bold text-2xl"
       />
       <div className="h-full absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-6 justify-end flex flex-col">
         <h3 className="text-2xl font-semibold text-white">{nombre}</h3>
