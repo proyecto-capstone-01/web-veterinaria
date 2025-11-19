18f75e9dcb3dd6f2fb507a64d429d92220041ac3
Sebastián G <seba.gallardog@duocuc.cl>
2025-11-19 18:49:09 -0300

    fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 import { webcore } from 'webcoreui/integration';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
+import process from 'node:process';
 
 const isNode = process.env.ASTRO_ADAPTER === 'node';
 
+console.log(`Using Astro adapter: ${process.env.ASTRO_ADAPTER}`);
+
+console.log(isNode ? 'Running with Node adapter' : 'Running with Cloudflare adapter');
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({}) ,
-  prefetch: true,
-  vite: {
-      // @ts-ignore
-    plugins: [tailwindcss()],
-      css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler'
-        }
+    env: {
+      schema: {
+          CMS_API_URL: envField.string({ context: 'server', access: 'secret', default: 'http://localhost:3000' }),
+          CMS_API_KEY: envField.string({ context: 'server', access: 'secret', default: '' }),
+          PUBLIC_CONTACT_API_URL: envField.string({ context: 'client', access: 'public', default: 'http://localhost:3000' }),
+          OVERWRITE_FAQ: envField.boolean({ context: 'server', access: 'public', default: false }),
+          IMAGE_OPTIMIZATION: envField.boolean({ context: 'server', access: 'public', default: false }),
+          ASTRO_ADAPTER: envField.string({ context: 'server', access: 'public', default: 'cloudflare' }),
+
       }
     },
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      alias:  {
-        "react-dom/server": "react-dom/server.edge",
-      } ,
+    adapter: isNode ? node({mode: 'standalone'}) : cloudflare(),
+    prefetch: true,
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler'
+                }
+            }
+        },
+        resolve: {
+            // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+            alias: isNode ? {
+                "react-dom/server": "react-dom/server.edge",
+            } : undefined,
+        },
     },
-  },
 
-  integrations: [react({experimentalDisableStreaming: true}), webcore()],
+    integrations: [react({experimentalDisableStreaming: true}), webcore()],
 });
